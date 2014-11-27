4201f090f0ea25f2d23f67ea756b006dddaf4968
Nicolas Limage <github@xephon.org>
2014-11-27 17:36:00 +0100

    python3 compat fixes

--- firval/__init__.py
+++ firval/__init__.py
@@ -603,26 +603,24 @@ def main():
     command-line version of the lib
     """
     try:
-        desc = 'a netfilter firewall rules generator ' + \
-               'designed to be easy to read, write and maintain'
-        parser = argparse.ArgumentParser(description=desc)
-        parser.add_argument('file', type=str, help='rules file',
-                            nargs='?', default='-')
+        parser = argparse.ArgumentParser()
+        parser.add_argument('file', type=str, nargs='?', default='-',
+                            help='a yaml rules file')
         args = parser.parse_args()
         if args.file == '-':
-            print str(Firval(yaml.load(sys.stdin)))
+            print(str(Firval(yaml.load(sys.stdin))))
         else:
             with open(args.file, 'r') as fd:
-                print str(Firval(yaml.load(fd)))
+                print(str(Firval(yaml.load(fd))))
     except yaml.parser.ParserError as ex:
-        print "# firval: yaml parsing error: " + str(ex).replace("\n", "")
+        print("# firval: yaml parsing error: " + str(ex).replace("\n", ""))
     except MultipleInvalid as ex:
-        print "# firval: config structure error: " + str(ex).replace("\n", "")
+        print("# firval: config structure error: " + str(ex).replace("\n", ""))
     except ParseError as ex:
-        print "# firval: rule parsing error: " + str(ex).replace("\n", "")
+        print("# firval: rule parsing error: " + str(ex).replace("\n", ""))
     except ConfigError as ex:
-        print "# firval: config error: " + str(ex).replace("\n", "")
+        print("# firval: config error: " + str(ex).replace("\n", ""))
     except KeyboardInterrupt as ex:
-        print "# firval: keyboard interrupted"
+        print("# firval: keyboard interrupt")
     except Exception as ex:
-        print "# firval: error: " + str(ex).replace("\n", "")
+        print("# firval: error: " + str(ex).replace("\n", ""))
